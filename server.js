940e55a3b1edea05d3bd5437d916d5df3f21515f
stephrrcodes <98715697+stephrrcodes@users.noreply.github.com>
2022-05-08 21:24:40 -0700

    added notes.html and server.js updated

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
+// port system
+const PORT = process.env.PORT || 3001;
+
+// invoke express app
+const app = express();
+
+// dependencies
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+// set up url parsing, json data and static files
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+//
+
+
+//deploy to heroku
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
