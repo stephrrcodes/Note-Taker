6037c3cb3bed8885022f68739df0c960a8b3ea54
stephrrcodes <98715697+stephrrcodes@users.noreply.github.com>
2022-05-08 22:45:33 -0700

    API and HTML routes added

--- server.js
+++ server.js
@@ -1,21 +1,48 @@
+const express = require ('express');
 // port system
 const PORT = process.env.PORT || 3001;
 
-// invoke express app
 const app = express();
 
-// dependencies
-const express = require('express');
+//
 const fs = require('fs');
 const path = require('path');
+const uuid = require('uuid')
+const notes = require('./db/db.json')
 
 // set up url parsing, json data and static files
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-//
+// API route "GET" request
+app.get('/api/notes', (req,res) => {
+    res.sendFile(path.join(__dirname, "/db/db.json"))
+});
 
+//API route "POST" request
+app.post ("/api/notes", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"))});
+    const newNotes= re.body;
+    newNotes.id = uuid.v4();
+    notes.push(newNotes);
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes))
+    res.json(notes);
+
+//API route "DELETE" request
+app.delete
+
+
+// HTML Routes
+
+// calls index.html
+app.get("/", function (req,res){
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+// calls notes.html
+app.get("/notes", function (req,res){
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
 
 //deploy to heroku
 app.listen(PORT, () => {
